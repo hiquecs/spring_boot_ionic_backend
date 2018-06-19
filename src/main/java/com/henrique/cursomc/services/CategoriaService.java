6cb74790ef738aa663b98ac60d623a311e028211
João Henrique <hiquecs@gmail.com>
2018-06-19 01:14:54 -0300

    Inserinco uma nova Categoria com Post

--- src/main/java/com/henrique/cursomc/services/CategoriaService.java
+++ src/main/java/com/henrique/cursomc/services/CategoriaService.java
@@ -11,13 +11,18 @@ import com.henrique.cursomc.services.Exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		}
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }

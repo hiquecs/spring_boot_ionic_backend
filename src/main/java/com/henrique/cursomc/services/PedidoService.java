b30d03c38f622fc07fc39b91673e8b69810595b8
João Henrique <hiquecs@gmail.com>
2018-07-16 19:39:06 -0300

    Email HTML

--- src/main/java/com/henrique/cursomc/services/PedidoService.java
+++ src/main/java/com/henrique/cursomc/services/PedidoService.java
@@ -62,7 +62,7 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
         
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }

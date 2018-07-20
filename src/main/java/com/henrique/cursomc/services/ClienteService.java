50a2460cb9dcb4d69ab3094947d5f940c8e20fdd
João Henrique <hiquecs@gmail.com>
2018-07-20 00:46:55 -0300

    Enviando imagem via endpoint

--- src/main/java/com/henrique/cursomc/services/ClienteService.java
+++ src/main/java/com/henrique/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.henrique.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.henrique.cursomc.DTO.ClienteDTO;
 import com.henrique.cursomc.DTO.ClienteNewDTO;
@@ -28,21 +30,25 @@ import com.henrique.cursomc.services.Exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
+	@Autowired
+
+	private S3Service s3Service;
+
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
-		if (user == null || !user.hasRole(Perfil.ADMIN) && ! id.equals(user.getId()) ) {
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
-			
+
 		}
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -59,7 +65,7 @@ public class ClienteService {
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		obj= repo.save(obj);
+		obj = repo.save(obj);
 		enderecoRepository.saveAll(obj.getEndereco());
 		return obj;
 	}
@@ -85,12 +91,12 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null,null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
 
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
-				TipoCliente.toEnum(objDto.getTipo()),pe.encode(objDto.getSenha()));
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), cli, cid);
@@ -112,5 +118,10 @@ public class ClienteService {
 
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+
+		return s3Service.uploadFile(multipartFile);
 	}
 }
